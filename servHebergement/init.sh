edf6fed2809ba37251672d7141c7c55c68b887ad
CanuAntoine <antoinecanu.pro78@gmail.com>
2025-10-10 11:35:55 +0200

    WIP

---
+++ servHebergement/init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "=========================================="
+echo "  Début du script d'initialisation"
+echo "=========================================="
+
+echo "MAJ des paquets et installation d'Ansible..."
+sudo apt update > /dev/null
+    || echo "Problème lors de la MAJ des paquets"; echo 1
+sudo apt install -y ansible
+    || echo "Problème lors de l'installation d'Ansible"; echo 1
+echo "Ansible installé"
+
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
